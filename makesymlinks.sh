177debb0fb888d9fb1fbf94e4396e10ef5de5ba9
Kwang Ketcham <prototype27+github@prototype27.com>
2016-04-26 21:00:36 -0700

    Don't require dotfiles to be cloned into the user's homedir

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,20 +6,15 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir=`dirname $0`                  # dotfiles directory
+olddir=$dir/dotfiles_old          # old dotfiles backup directory
 files="vimrc zshrc zshenv gitconfig gitignore htoprc"    # list of files/folders to symlink in homedir
 
 ##########
 
-# create dotfiles_old in homedir
+# create dotfiles_old
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
